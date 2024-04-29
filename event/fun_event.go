a360b714c81feade9b4eab39c03a8df0ef0b70af
agarkov.pavel3 <agarkov.pavel.ru1@gmail.com>
2024-04-30 01:37:21 +0600

    3

--- event/fun_event.go
+++ event/fun_event.go
@@ -1,21 +1,21 @@
-package event
-
-var FunListeningEvent ListeningEvent = "event.fun"
-
-func NewFunEvent() *FunEvent {
-	return &FunEvent{
-		name: FunListeningEvent,
-	}
-}
-
-type FunEvent struct {
-	name ListeningEvent
-}
-
-func (fe *FunEvent) GetName() ListeningEvent {
-	return fe.name
-}
-
-func (fe *FunEvent) GetData() string {
-	return ""
-}
+//package event
+//
+//var FunListeningEvent ListeningEvent = "event.fun"
+//
+//func NewFunEvent() *FunEvent {
+//	return &FunEvent{
+//		name: FunListeningEvent,
+//	}
+//}
+//
+//type FunEvent struct {
+//	name ListeningEvent
+//}
+//
+//func (fe *FunEvent) GetName() ListeningEvent {
+//	return fe.name
+//}
+//
+//func (fe *FunEvent) GetData() string {
+//	return ""
+//}
--- event/order_event.go
+++ event/order_event.go
@@ -1,21 +1,21 @@
-package event
-
-var OrderListeningEvent ListeningEvent = "event.order"
-
-func NewOrderEvent() *OrderEvent {
-	return &OrderEvent{
-		name: OrderListeningEvent,
-	}
-}
-
-type OrderEvent struct {
-	name ListeningEvent
-}
-
-func (oe *OrderEvent) GetName() ListeningEvent {
-	return oe.name
-}
-
-func (oe *OrderEvent) GetData() string {
-	return ""
-}
+//package event
+//
+//var OrderListeningEvent ListeningEvent = "event.order"
+//
+//func NewOrderEvent() *OrderEvent {
+//	return &OrderEvent{
+//		name: OrderListeningEvent,
+//	}
+//}
+//
+//type OrderEvent struct {
+//	name ListeningEvent
+//}
+//
+//func (oe *OrderEvent) GetName() ListeningEvent {
+//	return oe.name
+//}
+//
+//func (oe *OrderEvent) GetData() string {
+//	return ""
+//}
